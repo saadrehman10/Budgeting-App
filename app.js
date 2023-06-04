cb05d5f2322995cb5297453d063cd361c5dfd059
Saad ur Rehman <125679304+saadrehman10@users.noreply.github.com>
2023-06-04 06:20:36 +0500

    saad

---
+++ app.js
@@ -0,0 +1,29 @@
+function balanceAdd(){
+     let balance = +prompt("Enter the amount you want to add to your balance,\nShould be less than 999999999");
+     console.log(balance);
+     if( typeof(balance) === "number" && balance > 0 && balance <= 999999999){
+        currentBugget.innerHTML = `Rs. ${balance}`;
+     }
+     else{
+        currentBugget.innerHTML = "Rs. 0";
+        alert("Please enter a valid amount");
+        location.reload();
+     }
+}
+let expencesList = [];
+function addExpences(){
+    let sele = document.getElementById("selector1").value;
+    let amt = +document.getElementById("Amount").value;
+    let date = document.getElementById("date").value;
+    let desc = document.getElementById("description").value;
+    if (amt <= 0 || date === "" || desc === ""|| isNaN(amt)){
+         alert("Please enter a valid amount");
+
+    }
+    else{
+      let expence = { sele, amt, date, desc };
+      expencesList.push(expence);
+      console.log(expence);
+      console.log(expencesList);
+   }
+}
